d4e0ee960dcfea20d25585fcfa41fea27172ae11
PlaySmart-Kids <playsmart.kids@gmail.com>
2024-08-26 17:27:56 +0000

    Automatic update from bitbucket

--- assets/preview-DVWA7F1V.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe-BqF9LBwA.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-B6_a7-_E.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-B6_a7-_E.js","./iframe-BqF9LBwA.js","./index-BwDkhjyp.js","./_commonjsHelpers-BosuxZz1.js","./react-18-CdTkaE-n.js","./index-B8XB3FuZ.js","./_getPrototype-C0MsqmOH.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
